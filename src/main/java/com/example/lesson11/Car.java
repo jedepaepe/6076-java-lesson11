443e06098c45e8ee4d8485c1d170c06e470eaab8
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-12-04 00:08:55 +0100

    gitignore

---
+++ src/main/java/com/example/lesson11/Car.java
@@ -0,0 +1,24 @@
+package com.example.lesson11;
+
+import java.time.Year;
+
+public class Car {
+    String brand;
+    String model;
+    String licensePlate;
+    String color;
+    double buyPrice;
+    int constructionYear;
+
+    int getAge() {
+        return Year.now().getValue() - constructionYear;
+    }
+
+    double getCurrentPrice() {
+        return Math.max(0, buyPrice * (1 - getAge() * 0.1));
+    }
+
+    boolean equals(Car car) {
+        return licensePlate.equals(car.licensePlate);
+    }
+}
